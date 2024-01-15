76eecb3d6015df9f9757ea56fb1d11efa75d123d
Eerik Abel <eerikabel33@gmail.com>
2024-01-15 23:51:17 +0200

    improved posting

--- server/chat_server.py
+++ server/chat_server.py
@@ -1,13 +1,15 @@
 import socket
 import os
+import time
 
+print("Server started. Press Ctrl+Z to close")
 directory = os.path.dirname(__file__)+"/chatserverlog.txt"
 s = socket.socket()
-print("socket created")
 
 s.bind(("localhost",9999))
+print("socket created")
 
-s.listen(5)
+s.listen(1)
 print("Waiting for connection")
 
 while True:
@@ -18,7 +20,7 @@ while True:
     #c.send(bytes(msg,"utf-8"))
 
     with open(directory,"a") as file:
-        if msg != "default":
+        if msg == "post":
             file.write("\n"+msg)
 
     if msg != "login":
@@ -28,5 +30,6 @@ while True:
             c.send(bytes(text,"utf-8"))
             for line in msgs:
                 c.send(bytes(line,"utf-8"))
+                
 
     c.close()
