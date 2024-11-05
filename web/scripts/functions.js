57be771b046aefb727996d6850ab4e70dea40359
Eerik Abel <eerikabel33@gmail.com>
2024-11-05 12:38:12 +0200

    Log in from website

--- web/scripts/functions.js
+++ web/scripts/functions.js
@@ -1,25 +1,33 @@
-//Functions shared between pages
+///Functions shared between pages
 
+//Declare variable
 var username;
 var password;
+var pass_hash;
+var userid;
 
 //import {Server} from "socket.io";
 //var net = require("net");
 const url = "127.0.0.1";
 const port = 9000;
-const socket = new WebSocket("ws://localhost:9000");
 
+var server_version = sessionStorage.getItem("version");
+
+const error = document.querySelector(".error");
 
 function get_userdata()
 {
     //Get user credentials
     username = sessionStorage.getItem("username");
-    password = sessionStorage.getItem("password");
+    userid = sessionStorage.getItem("userid");
+    pass_hash = sessionStorage.getItem("password");
 }
 
-function request(text)
+function request_raw(text,func)
 {
-    console.log("request");
+    error_reset()
+    console.log("request",text);
+    const socket = new WebSocket("ws://localhost:9000");
 
     //Request data from server
     /*var client = new net.Socket(); //Net socket
@@ -38,13 +46,66 @@ function request(text)
     socket.addEventListener("open", function() {
         socket.send(text);
     });
+
+    socket.addEventListener("error",function() {
+        error.textContent = "Could not connect to server";
+    })
       
     // Listen for messages
     socket.addEventListener("message", (event) => {
-        console.log("Message from server ", event.data);
-    });
+        var resp = event.data.toString();
+        console.log("Message from server ", resp);
 
+        //If the command is successful, execute function, otherwise write out the error
+        if (resp.substring(0,5) === "Error" || resp.substring(0,12) === "Server error")
+        {
+            error.textContent = resp;
+        }
+        else 
+        {
+            func(resp);
+        }
+    });
+}
+
+async function hashing(text)
+{
+    //Why is hashing so complicated here?
+    const enc = new TextEncoder().encode(text); // encode as (utf-8) Uint8Array
+    const hash = await window.crypto.subtle.digest("SHA-256", enc); // hash the message
+    const hashArray = Array.from(new Uint8Array(hash)); // convert buffer to byte array
+    const hex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join(""); // convert bytes to hex string
+    return hex;
+
+    /*Using the command:
+    hashing(text).then((hash) => {
+        hash;
+    });
+    */
+}
+
+async function hash_password(username,password)
+{
+    pass_txt = "[user]"+username+"[pass]"+password;
+    return await hashing(pass_txt);
+}
+
+function request_user(cmd,txt)
+{
+    
+}
+
+function error_set(txt)
+{
+    console.log(txt);
+    error.textContent = txt;
+}
+
+function error_reset()
+{
+    error.textContent = "";
 }
 
 get_userdata();
-request("version");
--- web/scripts/login.js
+++ web/scripts/login.js
@@ -1,69 +1,118 @@
-//Scripting for Login and Register pages
+///Scripting for Login and Register pages
 
 //Clear user credentials from storage
 sessionStorage.removeItem("username");
+sessionStorage.removeItem("userid");
 sessionStorage.removeItem("password");
 
-function login()
+username = "";
+password = "";
+pass_hash = "";
+userid = "";
+var email = "";
+
+const entry_user = document.getElementById("name");
+const entry_email = document.getElementById("email");
+const entry_password = document.getElementById("password");
+const entry_confirm = document.getElementById("confirm");
+
+function check_password()
 {
-    const entry_user = document.getElementById("name");
-    const entry_password = document.getElementById("password");
-
-    username = entry_user.value;
-    password = entry_password.value;
-
-    //Save values
-    sessionStorage.setItem("username",username);
-    sessionStorage.setItem("password",password);
-
-    location.href = "posts.html"; //Go to next page
-}
-
-function register()
-{
-    const entry_user = document.getElementById("name");
-    const entry_email = document.getElementById("email");
-    const entry_password = document.getElementById("password");
-    const entry_confirm = document.getElementById("confirm");
-
-    username = entry_user.value;
-    password = entry_password.value;
-    
     //Check if passwords match
-    if (password != entry_confirm.value)
+    if (entry_password.value !== entry_confirm.value)
     {
         entry_confirm.setCustomValidity("Passwords do not match");
-        return;
     }
     else
     {
         entry_confirm.setCustomValidity("");
     }
+}
 
+function login(resp)
+{
     //Save values
+    userid = resp;
     sessionStorage.setItem("username",username);
-    sessionStorage.setItem("password",password);
+    sessionStorage.setItem("userid",userid);
+    sessionStorage.setItem("password",pass_hash);
 
     location.href = "posts.html"; //Go to next page
-    
 }
 
-//Add function to submit buttons, if existing
-const reg_form = document.getElementById("regform");
-const log_form = document.getElementById("logform");
-
-if (reg_form)
+function submit_login()
 {
-    reg_form.addEventListener("submit",(event) => {
-        register();
-        event.preventDefault();
+    username = entry_user.value;
+    password = entry_password.value;
+    error_reset();
+
+    //Error checking
+    if (username === "" || password === "")
+    {
+        error_set("Please fill all fields");
+        return;
+    }
+
+    //Hash password and submit data
+    hash_password(username,password).then(function(hash){
+        request_raw("login\n"+username+"\n"+hash,login);
     })
 }
 
-if (log_form)
-{    
-    log_form.addEventListener("submit",(event) => {
-        login();
-        event.preventDefault();
-    })
+function submit_register()
+{
+    username = entry_user.value;
+    email = entry_email.value;
+    password = entry_password.value;
+    confirm_value = entry_confirm.value;
+    error_reset();
+
+    //Error checking
+    //Custom messages are used over Validity, because the messages can be slightly hard to find
+    if (username === "" || password === "" || email === "" || confirm_value === "")
+    {
+        error_set("Please fill all fields");
+        return;
+    }
+    if (username.length < 4 || username.length > 32)
+    {
+        error_set("Username must be 4-32 characters");
+        return;
+    }
+    if (password.length < 8 || password.length > 64)
+    {
+        error_set("Username must be 8-64 characters");
+        return;
+    }
+    if (password !== confirm_value)
+    {
+        error_set("Passwords do not match");
+        return;
+    }
+    if (entry_email.validity.typeMismatch)
+    {
+        error_set("Invalid email format");
+        return;
+    }
+    if (entry_user.validity.patternMismatch)
+    {
+        error_set("Username contains invalid characters");
+        return;
+    }
+
+    //Hash sensitive data and submit user
+    hash_password(username,password).then(function(pass_hash){
+        hashing(email).then(function(email_hash){
+        request_raw("register\n"+username+"\n"+pass_hash+"\n"+email_hash,login);
+    })});
 }
+
+//Get server version
+function get_version(resp)
+{
+    server_version = resp;
+    sessionStorage.setItem("version",resp);
+}
+
+if (!server_version)
+    request_raw("version",get_version);
